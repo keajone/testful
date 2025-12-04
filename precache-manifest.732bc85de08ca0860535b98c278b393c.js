9b0d790498ddafec0060b514a853877189ed770a
keajone <keatonjones44@gmail.com>
2025-12-04 14:27:36 -0500

    Updates

---
+++ precache-manifest.732bc85de08ca0860535b98c278b393c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08498690a82368654b59e8e149a4680",
+    "url": "/testful/index.html"
+  },
+  {
+    "revision": "a460a524105e9e0beb43",
+    "url": "/testful/static/css/2.cc930a2b.chunk.css"
+  },
+  {
+    "revision": "7db627086ab83bb91095",
+    "url": "/testful/static/css/main.18c6508e.chunk.css"
+  },
+  {
+    "revision": "a460a524105e9e0beb43",
+    "url": "/testful/static/js/2.50488c46.chunk.js"
+  },
+  {
+    "revision": "23868572a7ff9e7c751dd5808c9fc52e",
+    "url": "/testful/static/js/2.50488c46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db627086ab83bb91095",
+    "url": "/testful/static/js/main.26a5b3e2.chunk.js"
+  },
+  {
+    "revision": "10bf25c2fa23b41c61cf",
+    "url": "/testful/static/js/runtime-main.ef421bf5.js"
+  }
+]);
